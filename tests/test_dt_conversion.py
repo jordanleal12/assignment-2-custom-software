34a9075520110f07dc940ed2e8d0e83b91ec743c
Jordan Leal-Walker <16386@coderacademy.edu.au>
2025-06-25 15:42:48 +1000

    - Added function to test for no returned or invalid co-ordinates for get_timezone function - Update test case docstring to clarify PYTHONPATH requirement

--- tests/test_dt_conversion.py
+++ tests/test_dt_conversion.py
@@ -1,4 +1,4 @@
-"""Test cases for the dt_conversion module."""
+"""Test cases for the dt_conversion module. PYTHONPATH=src must be input in terminal to run this file."""
 
 # import pytz
 # import pytest
@@ -11,4 +11,19 @@ def test_get_timezone_missing_keys(capsys):
     tz = dtc.get_timezone({})
     captured = capsys.readouterr()
     assert "Coordinates must contain 'lat' and 'lon'" in captured.out
+    assert tz == "UTC"
+
+def test_get_timezone_invalid_coords(capsys, monkeypatch):
+    """Passing invalid coordinates should print an error and return 'UTC'"""
+
+    class DummyTF:
+        """Dummy TimezoneFinder class to return none."""
+        def timezone_at(self, lat, lng):
+            """Return None for any coordinates."""
+            return None
+    monkeypatch.setattr(dtc, "TimezoneFinder", lambda: DummyTF())
+
+    tz = dtc.get_timezone({"lat": 0, "lon": 0})
+    captured = capsys.readouterr()
+    assert "Coordinates do not correspond to a valid timezone" in captured.out
     assert tz == "UTC"
